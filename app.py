911fcad7bb8728dd9771d4a843584d57fe8c03bd
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2019-05-26 22:31:54 +0200

    updated ip_address field and erros flash

--- app.py
+++ app.py
@@ -6,18 +6,17 @@ from flask import Flask, render_template, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Email, IPAddress
-from save_config import save_data
+from wtforms.validators import DataRequired, Email, IPAddress, Optional
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '00b596b981ee3a3d89afbd5b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 class LoginForm(FlaskForm):
     ssid = StringField('Network Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     email = EmailField('Email address', validators=[DataRequired(), Email()])
-    ip_address = StringField('Server IP address', validators=[IPAddress()])
+    ip_address = StringField('Server IP address', validators=[Optional(), IPAddress()])
     submit = SubmitField('Submit')
 
 
@@ -26,24 +25,24 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('requested for user {}, {}'.format(
-                form.ssid.data, form.email.data))
-        
+            form.ssid.data, form.email.data))
+        print(form.ssid.data, form.email.data, form.ip_address.data)
         ssid = form.ssid.data
         password = form.password.data
         wifi_connect(ssid, password)
         sys.exit(0)
         return redirect('/')
+    else:
+        for field, error in form.errors.items():
+            flash(f'Troubles with field {getattr(form, field).label.text}: {" ".join(error)}')
     return render_template('config.html', title='config', form=form)
 
 
 def wifi_connect(ssid, password):
     subprocess.run(f'./wifi_connection.sh {ssid} {password}', shell=True)
 
+
 if __name__ == '__main__':
     subprocess.Popen('sudo ./start_hotspot.sh', shell=True)
     time.sleep(10)
     app.run(host='192.168.0.10')
-
-
-
-
